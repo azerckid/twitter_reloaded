81ec0ce958a6d33b1f4f0385869d4407116597e2
azerckid <azerckid@gmail.com>
2025-04-20 16:10:12 +0900

    refactor: Move Firebase config to environment variables

--- src/routes/firebase.ts
+++ src/routes/firebase.ts
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAfYgQHfWXoDe2Cs8y6Q0aWsjznAQHAQSM",
-    authDomain: "react-twitter-clone-d017e.firebaseapp.com",
-    projectId: "react-twitter-clone-d017e",
-    storageBucket: "react-twitter-clone-d017e.firebasestorage.app",
-    messagingSenderId: "1091596428000",
-    appId: "1:1091596428000:web:5c20784ddc3d8e2d41a685"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
